934407f00d93b680e0093c0502ecfd4dd6ce39c3
Amogh-lab <amoghap099@gmail.com>
2024-12-20 12:33:37 +0530

    inputField

--- Typescript-react/src/App.tsx
+++ Typescript-react/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./App.css"
 import Inputfield from './Components/inputfield'
 
 const App: React.FC= ()=> {
+  const [todo, setTodo] = useState<string>("")
   return (
     <div className="topgap">
       <div className='hero'>
       <h1>Taskify</h1>
       </div>
-      <Inputfield />
+      <Inputfield todo={todo} setTodo={setTodo}/>
     </div>
   )
 }
--- Typescript-react/src/Components/inputfield.tsx
+++ Typescript-react/src/Components/inputfield.tsx
@@ -1,6 +1,10 @@
 import "./inputbox.css"
-
-const inputfield = () => {
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;           //todo is a string used for storing todo list and setTodo is the function for it
+}
+//typedeff for todo and setTodo using interface
+const inputfield = ({todo, setTodo}: props) => {                   //const inputfield: React.FC<props>= ({todo, setTodo}) instead we can also use this
   return (
     <form className="Inputlist">
         <input type='input' placeholder="Task to do" className="Todobox"/>
