7a3ba4c46f979a59bd1762a1241126abaa621620
Amogh-lab <amoghap099@gmail.com>
2024-12-21 01:52:47 +0530

    todo error correction

--- Typescript-react/src/App.tsx
+++ Typescript-react/src/App.tsx
@@ -5,16 +5,18 @@ import {Todo} from './model'
 
 const App: React.FC= ()=> {
   const [todo, setTodo] = useState<string>("")
-  const [todos, setTodos] = useState<Todo>([])
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const handleAdd = (e : React.FormEvent) => {
      e.preventDefault();
 
      if(todo){
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-
-     }
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);               //{ id: Date.now(), todo, isDone: false } this part adds new task to the array todos
+      setTodo("");                                                                 //todos will be of the form todos = [{id: 1, todo: "Task 1", isDone: false}]
+    }
   };
+  console.log(todo);
+  console.log(todos);
   return (
     <div className="topgap">
       <div>
