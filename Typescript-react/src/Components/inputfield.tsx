b583b5782d2d1c40ad5d424f2becaf2a021615c4
Amogh-lab <amoghap099@gmail.com>
2025-01-12 13:08:20 +0530

    css

--- Typescript-react/src/Components/inputfield.tsx
+++ Typescript-react/src/Components/inputfield.tsx
@@ -27,7 +27,7 @@ const Inputfield: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
         className="input__box"
       />
       <button type="submit" className="input_submit">
-        GO
+        Enter
       </button>
     </form>
   );
